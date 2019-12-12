dfa51bb1635fe21a746508be3e951a3e495a4bd9
Mate Bodor <bodormate@gmail.com>
2019-12-12 08:52:45 +0100

    get euro rate

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const button = document.querySelector("#calculate-fish");
 
 const calculateNetAmount = (gross) => Math.floor((gross / 127) * 100);
 
+
+const calculateEuro = async(amountInHuf) => {
+    const getCurrencyRate = await fetch("http: //www.apilayer.net/api/live?access_key=aa498a31f4a59ca219253f15c85fabb8&currencies=EUR,HUF&format=1")
+    const currencyJson = await getCurrencyRate.json();
+    return Math.floor(amountInHuf / currencyJson.quotes.USDHUF)
+};
+
+
 const fishMap = {
     smallFish: 5,
     bigFish: 50,
@@ -44,8 +52,11 @@ const renderFish = (fishes) => {
 
 const clickCalcualet = () => {
     const gross = document.querySelector('#gross-amount').value;
-    const net = calculateNetAmount(gross);
+    const euroGross = calculateEuro(gross);
+    const net = calculateNetAmount(euroGross);
     const fishes = calculateFish(net);
+    showValue('change-rate', currencyJson.quotes.USDHUF)
+    showValue('euro-gross', euroGross)
     showValue('net-amount', net);
     showValue('bigfish-amount', fishes.bigFishAmount);
     showValue('smallfish-amount', fishes.smallFishAmount);
