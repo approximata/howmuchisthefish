791da36ce6570433598653fd82066a46f31498a0
REALEYESIT\mate.kovacs <mate.kovacs@realeyesit.com>
2019-12-12 15:14:09 +0100

    add euro properly

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const button = document.querySelector("#calculate-fish");
 
 const calculateNetAmount = (gross) => Math.floor((gross / 127) * 100);
 
+let currencyJson;
 
 const calculateEuro = async(amountInHuf) => {
-    const getCurrencyRate = await fetch("http: //www.apilayer.net/api/live?access_key=aa498a31f4a59ca219253f15c85fabb8&currencies=EUR,HUF&format=1")
-    const currencyJson = await getCurrencyRate.json();
+    const getCurrencyRate = await fetch("http://www.apilayer.net/api/live?access_key=aa498a31f4a59ca219253f15c85fabb8&currencies=EUR,HUF&format=1")
+    currencyJson = await getCurrencyRate.json();
     return Math.floor(amountInHuf / currencyJson.quotes.USDHUF)
 };
-
 
 const fishMap = {
     smallFish: 5,
@@ -23,6 +23,9 @@ const calculateFish = (net) => {
         net -= bigFishAmount * fishMap.bigFish;
     }
     smallFishAmount = Math.floor(net / fishMap.smallFish);
+    if(bigFishAmount === 0 && smallFishAmount === 0) {
+        smallFishAmount = 1;
+    }
 
     return {bigFishAmount, smallFishAmount}
 }
@@ -49,18 +52,18 @@ const renderFish = (fishes) => {
     render(fishes.smallFishAmount, 'kisponty.jpg', 'smallfish-pic', 100);
 }
 
-
 const clickCalcualet = () => {
     const gross = document.querySelector('#gross-amount').value;
-    const euroGross = calculateEuro(gross);
-    const net = calculateNetAmount(euroGross);
-    const fishes = calculateFish(net);
-    showValue('change-rate', currencyJson.quotes.USDHUF)
-    showValue('euro-gross', euroGross)
-    showValue('net-amount', net);
-    showValue('bigfish-amount', fishes.bigFishAmount);
-    showValue('smallfish-amount', fishes.smallFishAmount);
-    renderFish(fishes);
+    calculateEuro(gross).then((euroGross) => {
+        const net = calculateNetAmount(euroGross);
+        const fishes = calculateFish(net);
+        showValue('change-rate', currencyJson.quotes.USDHUF)
+        showValue('euro-gross', euroGross)
+        showValue('net-amount', net);
+        showValue('bigfish-amount', fishes.bigFishAmount);
+        showValue('smallfish-amount', fishes.smallFishAmount);
+        renderFish(fishes);
+    });
 }
 
 button.addEventListener('click', clickCalcualet)
